94ca70349ddbb24bde5efdeef2c2e075b2897020
rickymsmith <35548840+rickymsmith@users.noreply.github.com>
2022-04-21 18:51:03 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+For loop circles, only red
---
+++ src/index.js
@@ -0,0 +1,29 @@
+class Box{
+    constructor(width, height, backgroundColor){
+        this.color = backgroundColor;
+        this.height = `${height}px`;
+        this.width = `${width}px`;
+        this.circle = '50%'
+    }
+
+    render () {
+        const body = document.body;
+        const div = document.createElement('div');
+        div.style.width = this.width;
+        div.style.height = this.height;
+        div.style.borderRadius = this.circle;
+        div.style.backgroundColor = this.color;
+        body.appendChild(div);
+        console.log('work')
+    }
+}
+
+const myBox = new Box(50,50,'red');
+
+console.log(myBox)
+
+for (let i = 0; i <= 9; i += 1){
+    myBox.render();
+}
+
+
